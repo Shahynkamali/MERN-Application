13eec55702c4a8810d9d61aef047e836430c2e19
Shahyn <shahynkamali@gmail.com>
2019-05-16 18:31:07 +0200

    started on auth on the backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Provider} from 'react-redux'
 import {Container} from 'reactstrap'
 import store from './store'
@@ -6,23 +6,32 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import AppNavBar from './Components/AppNavBar'
 import WineList from './Components/WineList'
 import ItemModel from './Components//ItemModel'
+import {loadUser} from './actions/authActions'
 
-import './App.css';
+import './App.css'
 
 
 
-function App() {
-  return (
-    <Provider store={store}>
-      <div>
-        <AppNavBar/>
-        <Container>
-          <ItemModel/>
-          <WineList/>
-        </Container>
-      </div>
-    </Provider>
-  );
+class App extends Component{
+  componentDidMount() {
+    store.dispatch(loadUser())
+  }
+  render(){
+    return (
+      <Provider store={store}>
+        <div>
+          <AppNavBar/>
+          <Container>
+            <ItemModel/>
+            <WineList/>
+          </Container>
+        </div>
+      </Provider>
+    );
+
+  }
 }
+
+
 
 export default App;
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const router = express.Router()
 const bcrypt = require('bcryptjs')
 const config = require('config')
 const jwt = require('jsonwebtoken')
-const router = express.Router()
+const auth = require('../../middleware/auth')
+
+
 const User = require('../../models/User')
 
 
@@ -10,45 +13,57 @@ router.post('/', (req, res) => {
     const {
         email,
         password
-    } = req.body
-    if ( !email || !password) {
+    } = req.body;
+
+
+    if (!email || !password) {
         return res.status(400).json({
-            message: 'please enter all fields!'
-        })
+            msg: 'Please enter all fields'
+        });
     }
+
+
     User.findOne({
             email
         })
         .then(user => {
             if (!user) return res.status(400).json({
-                message: 'user does not exists'
-            })
+                msg: 'User Does not exist'
+            });
 
-        bcrypt.compare(password, user.password)
-        .then(isMatch =>{
-            if(!isMatch) return res.status(400).json({message:"invalid credentials"})
-            jwt.sign({
-                    id: user.id
-                },
-                config.get('jwtSecret'), {
-                    expiresIn: 3600
-                },
-                (err, token) => {
-                    if (err) throw new Error('error')
-                    res.json({
-                        token,
-                        user: {
-                            id: user.id,
-                            name: user.name,
-                            email: user.email
+
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if (!isMatch) return res.status(400).json({
+                        msg: 'Invalid credentials'
+                    });
+
+                    jwt.sign({
+                            id: user.id
+                        },
+                        config.get('jwtSecret'), {
+                            expiresIn: 3600
+                        },
+                        (err, token) => {
+                            if (err) throw err;
+                            res.json({
+                                token,
+                                user: {
+                                    id: user.id,
+                                    name: user.name,
+                                    email: user.email
+                                }
+                            })
                         }
-                    })
-                }
-            )
+                    )
+                })
         })
-    })
 })
 
+router.get('/user', auth, (req, res) => {
+    User.findById(req.user.id)
+        .select('-password')
+        .then(user => res.json(user));
+});
 
-
-module.exports = router
+module.exports = router;
