d87287ef69c2baa603aa05b825d27f5869f57bac
Shahyn <shahynkamali@gmail.com>
2019-05-16 18:32:11 +0200

    auth started

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,11 +1,9 @@
 import axios from 'axios'
 import {returnErrors} from './errorActions'
-
 import {USER_LOADED, USER_LOADING, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT_SUCCESS, REGISTER_SUCCESS, REGISTER_FAIL} from './types';
 
-
 export const loadUser = () => (dispatch, getState) => {
- 
+
     dispatch({
         type: USER_LOADING
     })
